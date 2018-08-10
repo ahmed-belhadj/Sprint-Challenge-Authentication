fd5b24e6b66088867c10175de81c580cd4a78ed6
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2018-08-10 10:30:00 -0500

    Authentication Sprint Challenge
    register() /config/routes.js

--- config/routes.js
+++ config/routes.js
@@ -1,15 +1,40 @@
-const axios = require('axios');
+const axios = require("axios");
+const bcrypt = require("bcryptjs");
 
-const { authenticate } = require('./middlewares');
+const { authenticate, generateToken } = require("./middlewares");
+const db = require("../database/dbConfig");
 
 module.exports = server => {
-  server.post('/api/register', register);
-  server.post('/api/login', login);
-  server.get('/api/jokes', authenticate, getJokes);
+  server.post("/api/register", register);
+  server.post("/api/login", login);
+  server.get("/api/jokes", authenticate, getJokes);
 };
 
 function register(req, res) {
   // implement user registration
+  const user = req.body;
+
+  // hashes password
+  const hash = bcrypt.hashSync(user.password, 14);
+  user.password = hash;
+
+  db("users")
+    .insert(user)
+    .then(function(ids) {
+      db("users")
+        .where({ id: ids[0] })
+        .first()
+        .then(user => {
+          // generates the token
+          const token = generateToken(user);
+
+          // attaches the token to the response
+          res.status(201).json(token);
+        });
+    })
+    .catch(function(error) {
+      res.status(500).json({ error });
+    });
 }
 
 function login(req, res) {
@@ -19,12 +44,12 @@ function login(req, res) {
 function getJokes(req, res) {
   axios
     .get(
-      'https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_ten'
+      "https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_ten"
     )
     .then(response => {
       res.status(200).json(response.data);
     })
     .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+      res.status(500).json({ message: "Error Fetching Jokes", error: err });
     });
 }
